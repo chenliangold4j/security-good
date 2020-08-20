07ce5d2ff9c83916e4f290fd5db766d1144261ef
chenliang <chenliangxingji1@163.com>
2020-08-20 09:31:14 +0800

    keep

---
+++ tests/src/main/java/com/phantom5702/tests/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.phantom5702.tests.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/sgtest")
+@CrossOrigin
+public class TestController {
+
+    @GetMapping("/receivce")
+    public String test(HttpServletRequest httpServletRequest){
+        Cookie[] cookies = httpServletRequest.getCookies();
+        for(Cookie cookie:cookies){
+            System.out.println(cookie.getName()+":"+cookie.getValue());
+        }
+        return "test";
+    }
+
+}
